1dbf3699de06da62853000c49072772c2b031f58
SathyapriyaKaliaraj <127598916+SathyapriyaKaliaraj@users.noreply.github.com>
2023-10-12 20:39:49 +0530

    Create StandardInputRead.java

---
+++ StandardInputRead.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class StandarInputRead{
+  public static void main (String[] argd){
+    Scanner s=new Scanner(System.in);
+    System.out.println("Enter any no :");
+    int no=s.nextInt();
+  }
+}
